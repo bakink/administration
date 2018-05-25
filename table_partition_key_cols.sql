39735273a9045f263eac85488775d527023357f0
Baki Sahin <bakisahin@yahoo.com>
2018-05-25 11:13:34 +0300

    Update table_partition_key_cols.sql

--- table_partition_key_cols.sql
+++ table_partition_key_cols.sql
@@ -5,3 +5,11 @@ SELECT   owner, NAME, column_name, column_position
    WHERE owner LIKE UPPER ('&&ENTER_OWNER_NAME')
      AND NAME LIKE UPPER ('&&ENTER_TABLE_NAME')
 ORDER BY owner, NAME;
+
+--partition info
+select distinct tp.table_owner,tp.table_name,pt.partitioning_type,pt.SUBPARTITIONING_TYPE,pt.interval,count(1) 
+from dba_tab_partitions tp,DBA_PART_TABLES pt 
+where tp.table_owner=pt.owner and tp.table_name=pt.table_name and tp.table_owner not in ('SYS','SYSTEM') 
+group by tp.table_owner,tp.table_name,pt.partitioning_type,pt.SUBPARTITIONING_TYPE,pt.interval 
+order by 6 desc;
+
